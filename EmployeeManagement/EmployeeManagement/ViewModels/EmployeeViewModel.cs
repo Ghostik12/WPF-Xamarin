d29d5c5fbb4c54b6d6e737b92097e75d42a2c9ea
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2024-07-26 23:23:19 +0300

    TextBoxError

---
+++ EmployeeManagement/EmployeeManagement/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,61 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        private EmployeeRepository _employeeRepository;
+        private string _filter;
+        private ObservableCollection<Employee> _employees;
+
+        public EmployeeViewModel()
+        {
+            _employeeRepository = new EmployeeRepository();
+            FillListView();
+        }
+
+        public ObservableCollection<Employee> Employees 
+        { 
+            get 
+            { 
+                return _employees;
+            }
+            set
+            {
+                _employees = value;
+            }
+        }
+
+        public string Filter
+        {
+            get
+            {
+                return _filter;
+            }
+            set
+            {
+                _filter = value;
+            }
+        }
+
+        private void FillListView()
+        {
+            if (!String.IsNullOrEmpty(_filter))
+            {
+                _employees = new ObservableCollection<Employee>(
+                  _employeeRepository.GetAll().Where(v => v.FirstName.Contains(_filter)));
+            }
+            else
+                _employees = new ObservableCollection<Employee>(
+                  _employeeRepository.GetAll());
+        }
+
+    }
+}
